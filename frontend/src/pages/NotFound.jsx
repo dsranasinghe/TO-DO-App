acdd63b025f3e45f06d5cb419e9e871ec9b0f442
Dhanuji <dhanujiranasingha@gmail.com>
2025-03-28 09:15:29 +0530

    frontend form

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div>
+      <h1>404</h1>
+      <p>Not Found</p>
+      <p>Sorry, an error has occured, Requested page not found!</p>
+    </div>
+  )
+}
+
+export default NotFound
